1ecfe3f989d30138c0cd5307782ce5bd1e8c8c84
DrLuke <Lukas@Jackowski.de>
2018-04-11 19:56:54 +0200

    Rename Scene to Node

--- pyreeEngine/node.py
+++ pyreeEngine/node.py
@@ -3,29 +3,26 @@ from typing import Union, Type, List, Callable
 from pyreeEngine.engine import Framebuffer
 
 
-def signalInput(name:str, type:Union[Type, List[Type]], desc:str=""):
+def signalInput(name: str, type: Union[Type, List[Type]], desc: str=""):
     def decorator(func:Callable):
         obj = func.__self__
         obj.__signalInputs__[name] = (func, type, desc)
         return func
     return decorator
 
-def signalOutput(name:str, type:Union[Type, List[Type]], desc:str=""):
+def signalOutput(name: str, type: Union[Type, List[Type]], desc: str=""):
     def decorator(func:Callable):
         obj = func.__self__
         obj.__signalOutputs__[name] = (func, type, desc)
         return func
     return decorator
 
-class Scene:
+class Node:
     def __init__(self):
         self.__signalInputs__ = {}
         self.__signalOutputs__ = {}
 
     def init(self):
         pass
-
-    def render(self, fbo: Framebuffer):
-        fbo.bindFramebuffer()
 
 
