ab6729c2e2b2341f85d4361a45411b1a6a296a1b
DrLuke <Lukas@Jackowski.de>
2018-04-13 20:18:04 +0200

    Add nodeManager

---
+++ nodeManager.py
@@ -0,0 +1,46 @@
+from pathlib import Path
+
+import importlib
+
+
+"""
+Manages reloading nodes
+
+1: Detect all scenes within a project folder
+2: Load all scenes
+3: Install inotify to detect changes
+4: Run instances of classes in set order
+"""
+class NodeManager():
+    def __init__(self):
+        self.nodepath = None
+
+        self.layers = {}
+        self.classes = {}
+
+    def reloadNodes(self, path:Path):
+        pass
+
+    def loadNodeClasses(self, modulepath):
+        """Imports module and fetches all nodes from it
+
+        :param modulepath: Path to module (e.g. foo.bar.mymodule)
+        :type modulepath: string
+        :return: All node-classes from module or None on failure
+        :rtype: List[Class]
+        """
+        importlib.invalidate_caches()
+        try:
+            newmod = importlib.import_module(modulepath)
+        except ModuleNotFoundError:
+            print("ERRO: Module %s not found" % modulepath)
+        try:
+            classes = newmod.__nodeclasses__
+        except AttributeError:
+            print("ERROR: Module %s is missing '__nodeclasses__' attribute" % modulepath)
+            return None
+
+
+
+
+
