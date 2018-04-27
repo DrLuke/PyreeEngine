9be79dc9475a651404e9613a0ad30299d96de511
DrLuke <Lukas@Jackowski.de>
2018-04-28 00:36:06 +0200

    Add debug shader

--- pyreeEngine/engine.py
+++ pyreeEngine/engine.py
@@ -60,6 +60,54 @@ class DeferredShader():
 
         return DeferredShader.program
 
+
+class DebugShader():
+    vertexCode = """#version 450 core
+    layout (location = 0) in vec3 posIn;
+    layout (location = 1) in vec2 uvIn;
+    layout (location = 2) in vec3 normIn;
+
+    layout (location = 0) out vec3 posOut;
+    layout (location = 1) out vec2 uvOut;
+    layout (location = 2) out vec3 normOut;
+
+    uniform mat4 MVP;
+
+    void main()
+    {
+        gl_Position = MVP * vec4(posIn, 1);
+        posOut = (MVP * vec4(posIn, 1)).xyz;
+        uvOut = uvIn;
+        normOut = normIn;
+    }
+    """
+
+    fragCode = """#version 450 core
+    layout (location = 0) in vec3 posIn;
+    layout (location = 1) in vec2 uvIn;
+    layout (location = 2) in vec3 normIn;
+
+    layout (location = 0) out vec4 colorOut;
+    void main()
+    {
+        colorOut = vec4(uvIn, 0, 1);
+    }
+    """
+
+    vertShader = None
+    fragShader = None
+    program = None
+
+    @staticmethod
+    def getShaderProgram():
+        if DebugShader.program is None:
+            DebugShader.vertShader = shaders.compileShader(DebugShader.vertexCode, GL_VERTEX_SHADER)
+            DebugShader.fragShader = shaders.compileShader(DebugShader.fragCode, GL_FRAGMENT_SHADER)
+            DebugShader.program = shaders.compileProgram(DebugShader.vertShader, DebugShader.fragShader)
+
+        return DebugShader.program
+
+
 class PyreeObject():
     def __init__(self):
         self.children = []
