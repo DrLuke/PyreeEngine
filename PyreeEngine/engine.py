7f16f2caa699c2fbcb043d6f09c0fb722c091976
DrLuke <Lukas@Jackowski.de>
2018-11-19 23:39:19 +0100

    Update Engine to new layer system

--- PyreeEngine/engine.py
+++ PyreeEngine/engine.py
@@ -1,66 +1,18 @@
-from typing import Union, List, Tuple, Callable
+from typing import Union, List, Tuple, Callable, Dict
 import numpy as np
 import quaternion
 import math
 from PyreeEngine.util import Vec3
+from PyreeEngine.camera import *
 
 import glfw
+import ctypes
 import time
 
 from OpenGL.GL import *
 from OpenGL.GL import shaders
 
 import traceback, sys
-
-class DeferredShader():
-    """Singleton class containing the default shader for deferred rendering. This way the shader program only has
-    to exist once on the GPU."""
-    vertexCode = """#version 450 core
-    layout (location = 0) in vec3 posIn;
-    layout (location = 1) in vec2 uvIn;
-    layout (location = 2) in vec3 normIn;
-
-    layout (location = 0) out vec3 posOut;
-    layout (location = 1) out vec2 uvOut;
-    layout (location = 2) out vec3 normOut;
-
-    uniform mat4 MVP;
-
-    void main()
-    {
-        gl_Position = MVP * vec4(posIn, 1);
-        posOut = (MVP * vec4(posIn, 1)).xyz;
-        uvOut = uvIn;
-        normOut = normIn;
-    }
-    """
-
-    fragCode = """#version 450 core
-    layout (location = 0) in vec3 posIn;
-    layout (location = 1) in vec2 uvIn;
-    layout (location = 2) in vec3 normIn;
-    
-    layout (location = 0) out vec3 albedoOut;
-    layout (location = 1) out vec3 normalOut;
-    void main()
-    {
-        albedoOut = normIn; // TODO: Texturemapping!
-        normalOut = normIn;
-    }
-    """
-
-    vertShader = None
-    fragShader = None
-    program = None
-
-    @staticmethod
-    def getShaderProgram():
-        if DeferredShader.program is None:
-            DeferredShader.vertShader = shaders.compileShader(DeferredShader.vertexCode, GL_VERTEX_SHADER)
-            DeferredShader.fragShader = shaders.compileShader(DeferredShader.fragCode, GL_FRAGMENT_SHADER)
-            DeferredShader.program = shaders.compileProgram(DeferredShader.vertShader, DeferredShader.fragShader)
-
-        return DeferredShader.program
 
 
 class DebugShader():
@@ -271,63 +223,6 @@ class LaunchOptions:
 
         self.projectPath = None     # type: Path
 
-class Camera():
-    def __init__(self) -> None:
-        self.projectionMatrix = np.identity(4)
-        self.viewMatrix = None
-        self.pos = Vec3()
-
-        self.lookAt(np.array([0, 0, 0], np.float32), np.array([0, 0, -1], np.float32))
-
-    def lookAt(self, eye: np.array, target: np.array, up: np.array = np.array([0, 1, 0], np.float32)) -> None:
-        forward = target - eye
-        forward /= np.linalg.norm(forward)
-        up /= np.linalg.norm(up)
-        side = np.cross(forward, up)
-        up = np.cross(side, forward)
-
-        orientMat = np.transpose(np.matrix([[side[0], up[0], -forward[0], 0],
-                                            [side[1], up[1], -forward[1], 0],
-                                            [side[2], up[2], -forward[2], 0],
-                                            [0,       0,     0,          1]], np.float32))
-
-        translMat = np.matrix([[1, 0, 0, -eye[0]],
-                               [0, 1, 0, -eye[1]],
-                               [0, 0, 1, -eye[2]],
-                               [0, 0, 0, 1]], np.float32)
-
-        self.viewMatrix = orientMat * translMat
-
-class PerspectiveCamera(Camera):
-    def __init__(self) -> None:
-        super(PerspectiveCamera, self).__init__()
-
-        self.setPerspective(60, 640/480, 0.01, 100.)
-
-    def setPerspective(self, fovY, aspect, nearZ, farZ) -> None:
-        s = 1.0 / math.tan(math.radians(fovY) / 2.0)
-        sx, sy = s / aspect, s
-        zz = (farZ + nearZ) / (nearZ - farZ)
-        zw = 2 * farZ * nearZ / (nearZ - farZ)
-        self.projectionMatrix = np.matrix([[sx, 0, 0, 0],
-                                           [0, sy, 0, 0],
-                                           [0, 0, zz, zw],
-                                           [0, 0, -1, 0]])
-
-
-class OrthoCamera(Camera):
-    def __init__(self):
-        super(OrthoCamera, self).__init__()
-
-        self.setOrtho(1, 640/480, 0.01, 100.)
-
-    def setOrtho(self, sizeY, aspect, nearZ, farZ):
-        t,b = sizeY / 2, -sizeY / 2
-        r,l = t*aspect, b*aspect
-        self.projectionMatrix = np.matrix([[2/(r-l), 0,       0,               -(r+l)/(r-l)],
-                                           [0,       2/(t-b), 0,               -(t+b)/(t-b)],
-                                           [0,       0,       -2/(farZ-nearZ), -(farZ + nearZ)/(farZ - nearZ)],
-                                           [0,       0,       0,               1]])
 
 
 class NodeGlobalData():
@@ -342,29 +237,42 @@ class NodeGlobalData():
 
         self.otherData = {}
 
+class Monitor():
+    """Monitor abstraction
 
-from PyreeEngine.nodeManager import NodeManager
+    Stores name and videomodes of monitor, and can be updated on changes
+    """
+    def __init__(self, monitorptr: ctypes.POINTER(ctypes.POINTER(glfw._GLFWmonitor))):
+        if monitorptr is not None:
+            self.parse(monitorptr)
+
+    def parse(self, monitorptr: ctypes.POINTER(ctypes.POINTER(glfw._GLFWmonitor))):
+        self.monitorptr: ctypes.POINTER(ctypes.POINTER(glfw._GLFWmonitor)) = monitorptr
+
+        self.vidmodes = glfw.get_video_modes(self.monitorptr)
+        self.name = glfw.get_monitor_name(self.monitorptr)
+
+
+from PyreeEngine.layers import LayerContext, LayerManager, ProgramConfig
+import json
+import pythonosc.dispatcher
+import pythonosc.osc_server
+import pythonosc.udp_client
+import asyncio
+
 class Engine():
-    def __init__(self, config: LaunchOptions):
+    def __init__(self):
 
-        ####################
-        # TODO: Init glfw and opengl
-        glfw.init()     # TODO: Check if init successful, exit otherwise
+        self.initglfw()
 
-        glfw.window_hint(glfw.CONTEXT_VERSION_MAJOR, 4)
-        glfw.window_hint(glfw.CONTEXT_VERSION_MINOR, 5)
-        glfw.window_hint(glfw.OPENGL_PROFILE, glfw.OPENGL_CORE_PROFILE)
-        glfw.window_hint(glfw.SAMPLES, 4)
-        glfw.window_hint(glfw.AUTO_ICONIFY, False)
+        self.monitors: Dict[str, Monitor] = {}
+        self.monitors = self.getmonitors()
 
-        self.monitors = glfw.get_monitors()
+        resolution = (1280, 720)
 
-
-        self.window = glfw.create_window(1920, 1200, "PyreeEngine", self.monitors[1], None)
-        #self.window = glfw.create_window(1280, 720, "PyreeEngine", None, None)
-
+        #self.window = glfw.create_window(1920, 1200, "PyreeEngine", self.monitors[1], None)
+        self.window = glfw.create_window(resolution[0], resolution[1], "PyreeEngine", None, None)
         glfw.set_framebuffer_size_callback(self.window, self.framebufferResizeCallback)
-
 
         glfw.make_context_current(self.window)
 
@@ -373,51 +281,80 @@ class Engine():
 
         self.init()
 
-        ### Project management
-        self.globalData = NodeGlobalData()
-        self.globalData.resolution = [1280, 720]
-        self.globalData.time = glfw.get_time()
-        self.nodeMan = NodeManager(config.projectPath, self.globalData)
+        ## Program Config
+        with open("programconfig.json", "r") as f:
+            self.programconfig = ProgramConfig(**json.load(f))
 
-        while(not glfw.window_should_close(self.window)):
-            self.mainLoop()
+        ## OSC setup
+        self.oscdispatcher = pythonosc.dispatcher.Dispatcher()
+        self.oscserverloop = asyncio.get_event_loop()
+        self.oscserver = pythonosc.osc_server.AsyncIOOSCUDPServer((self.programconfig.oscserveraddress, self.programconfig.oscserverport), self.oscdispatcher, self.oscserverloop)
 
+        self.oscclient = pythonosc.udp_client.UDPClient(self.programconfig.oscclientaddress, self.programconfig.oscclientport)
+
+        ## Layer Context and Manager
+        self.layercontext: LayerContext = LayerContext()
+        self.layercontext.setresolution(resolution[0], resolution[1])
+        self.layercontext.time = glfw.get_time()
+
+        self.layercontext.oscdispatcher = self.oscdispatcher
+        self.layercontext.oscclient = self.oscclient
+
+        newtime = glfw.get_time()
+        self.layercontext.dt = newtime - self.layercontext.time
+        self.layercontext.time = newtime
+
+        self.layermanager: LayerManager = LayerManager(self.programconfig, self.layercontext)
+
+
+    def getmonitors(self) -> Dict[str, ctypes.POINTER(ctypes.POINTER(glfw._GLFWmonitor))]:
+        monitors = {}
+        for monitor in glfw.get_monitors():
+            newmonitor = Monitor(monitor)
+            monitors[newmonitor.name] = newmonitor
+        return monitors
+
+    def initglfw(self):
+        glfw.init()  # TODO: Check if init successful, exit otherwise
+
+        glfw.window_hint(glfw.CONTEXT_VERSION_MAJOR, 4)
+        glfw.window_hint(glfw.CONTEXT_VERSION_MINOR, 5)
+        glfw.window_hint(glfw.OPENGL_PROFILE, glfw.OPENGL_CORE_PROFILE)
+        glfw.window_hint(glfw.SAMPLES, 4)
+        glfw.window_hint(glfw.AUTO_ICONIFY, False)
 
     def init(self):
         pass
 
+    def startmainloop(self) -> None:
+        while (not glfw.window_should_close(self.window)):
+            self.mainLoop()
+
     def mainLoop(self) -> None:
-        # TODO: Query communications (midi, sockets)
-        # TODO: Check for changes in code
-        # TODO: Handle resize events from glfw or communications
         glfw.make_context_current(self.window)
 
         glfw.poll_events()
 
         newtime = glfw.get_time()
-        self.globalData.dt = newtime - self.globalData.time
-        self.globalData.time = newtime
+        self.layercontext.dt = newtime - self.layercontext.time
+        self.layercontext.time = newtime
 
         glClearColor(0.2, 0.2, 0.3, 1.)
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
 
-        glViewport(0, 0, self.globalData.resolution[0], self.globalData.resolution[1])
+        glViewport(0, 0, self.layercontext.resolution[0], self.layercontext.resolution[1])
 
         self.loop()
 
         glfw.swap_buffers(self.window)
 
-        self.globalData.resChanged = False
-
         glfw.swap_interval(1)
 
     def framebufferResizeCallback(self, window, width, height):
-        self.globalData.resChanged = True
-        self.globalData.resolution = [width, height]
-        self.globalData.aspect = width/height
+        self.layercontext.setresolution(width, height)
 
     def loop(self):
-        self.nodeMan.tick()
+        self.layermanager.tick()
 
     def render(self, objects: List[PyreeObject], camera: Camera, framebuffer) -> None:
         projectionMatrix = camera.projectionMatrix
