34b148418d66150e41376c8cd72fafb530fafb23
DrLuke <Lukas@Jackowski.de>
2018-12-11 22:50:26 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
-from distutils.core import setup
-
+from setuptools import setup
 from pathlib import Path
 
 with Path("VERSION").open("r") as f:
@@ -10,12 +9,26 @@ with Path("VERSION").open("r") as f:
 with Path("LICENSE").open("r") as f:
     license = f.read()
 
-setup(name='PyreeEngine',
-      version=version,
-      description='Pyree Engine',
-      author='Lukas Jackowski',
-      author_email='Lukas@Jackowski.de',
-      url='https://github.com/DrLuke/PyreeEngine/',
-      packages=['PyreeEngine'],
-      license=license
-      )
+setup(
+    name='PyreeEngine',
+    version=version,
+    description='Pyree Engine',
+    author='Lukas Jackowski',
+    author_email='Lukas@Jackowski.de',
+    url='https://github.com/DrLuke/PyreeEngine/',
+    packages=['PyreeEngine'],
+    license=license,
+    install_requires=[
+        "PyOpenGL",
+        "scipy",
+        "numpy",
+        "numpy-quaternion",
+        "glfw",
+        "numba",
+        "inotify_simple",
+        "Pillow",
+        "cffi",
+        "mido",
+        "python-rtmidi",
+    ]
+)
